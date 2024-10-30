7c8ac61017599c3ea50b67c576c62cd405ca548e
VitaFrancesco <vitafrancesco00@gmail.com>
2024-10-30 16:07:02 +0100

    ADD: snack-1 method .join

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,3 +3,5 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
+// console.log(...names);
+console.log(names.join(', '))
